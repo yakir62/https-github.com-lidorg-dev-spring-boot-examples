e3a1cc61bdd2cd4073bb45cae67f105c89a4aeb2
iritkatz <42677556+iritkatz@users.noreply.github.com>
2018-08-26 16:06:19 +0300

    Update ApplicationTests.java

--- spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
+++ spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
@@ -1,5 +1,5 @@
 package com.neo;
-
+import org.junit.jupiter.api.Test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
